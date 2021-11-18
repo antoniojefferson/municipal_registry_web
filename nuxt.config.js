889ef7d09ae6a6b4d275f3e6ddf9683bc6456c5d
Antonio Jefferson <antoniojeferson96@gmail.com>
2021-11-18 10:20:16 -0300

    Initializing repository

---
+++ nuxt.config.js
@@ -0,0 +1,22 @@
+export default {
+    mode: 'universal',
+
+    head: {
+        title: 'Registro de Municípios',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            {
+                hid: 'description',
+                name: 'description',
+                content: process.env.npm_package_description || ''
+            }
+        ],
+    },
+    css: [
+        '~/assets/stylesheet/scss/main.scss'
+    ],
+    plugins: [],
+    buildModules: [],
+    modules: []
+}

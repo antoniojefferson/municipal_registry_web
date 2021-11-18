7cf16c3aaa01d085a8d0fbca2075219a15d59e49
Antonio Jefferson <antoniojeferson96@gmail.com>
2021-11-18 10:30:04 -0300

    feat(layouts/defaults, pages/index): Criando a pagina inicial e o layout default.

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="main">
+        <Nuxt />
+    </div>
+</template>
+<style lang='scss'>
+.main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Index Page</h1>
+</template>
+<script>
+export default {
+    name: 'index',
+}
+</script>

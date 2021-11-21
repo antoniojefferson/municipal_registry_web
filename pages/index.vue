49961601d945e50b50033afb8f6d4e894a98a917
Antonio Jefferson <antoniojeferson96@gmail.com>
2021-11-21 09:36:08 -0300

    feat(pages/index.vue): Criando a pagina de entrada do app

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,29 @@
 <template>
-    <h4>Seja bem Vindo</h4>
+    <div>
+        <header-page title="Home Page" />
+        <div class="container">
+            <div class="mt--6">
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h1>Seja Bem Vindo</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+            </div>
+        </div>
+    </div>
 </template>
+<script>
+import HeaderPage from '../components/HeaderPage.vue';
+
+export default {
+    name: 'home',
+    components: {
+        HeaderPage
+    }
+}
+</script>

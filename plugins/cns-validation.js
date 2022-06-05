9182f5faaf7e753c241be36d014191bfb1e1be0a
Antonio Jefferson <antoniojeferson96@gmail.com>
2022-06-05 19:19:30 -0300

    refactor(plugins/*): corrigindo a identação do codigo

--- plugins/cns-validation.js
+++ plugins/cns-validation.js
@@ -5,66 +5,66 @@
     Obs: Compressed algorithms, but following the rules established in the reference above.
 */
 export default ({ app }, inject) => {
-    // Inject $cnsValidation(cns) in Vue, context and store.
+  // Inject $cnsValidation(cns) in Vue, context and store.
 
-    inject('cnsValidation', cns => {
-        let numCns = cns.replace(/\D/g, '');
+  inject('cnsValidation', cns => {
+    let numCns = cns.replace(/\D/g, '');
 
-        if (numCns.length != 15) {
-            return false;
+    if (numCns.length != 15) {
+      return false;
+    }
+
+    let sum = 0, rest, division, pis, result;
+
+    // Validate CNS
+    if ([1, 2].includes(parseInt(numCns.substring(0, 1)))) {
+      pis = numCns.substring(0, 11);
+
+      for (var i = 0; i < 11; i++) {
+        sum += parseInt(pis.substring(i, (i + 1)) * (15 - i));
+      }
+
+      rest = sum % 11;
+      division = 11 - rest;
+
+      if (division == 11) {
+        division = 0;
+      }
+
+      if (division == 10) {
+
+        for (var i = 0; i < 11; i++) {
+          sum += parseInt(pis.substring(i, (i + 1)) * (15 - i)) + ((15 - i) == 5 ? 2 : 0);
         }
 
-        let sum = 0, rest, division, pis, result;
+        rest = sum % 11;
+        division = 11 - rest;
+        result = pis + '001' + division.toString();
+      } else {
+        result = pis + "000" + division.toString();
+      }
 
-        // Validate CNS
-        if ( [1,2].includes(parseInt(numCns.substring(0,1))) ) {
-            pis = numCns.substring(0,11);
+      if (!(numCns == result)) {
+        return false;
+      } else {
+        return true;
+      }
+    }
 
-            for(var i = 0; i < 11; i++) {
-                sum += parseInt(pis.substring(i, (i + 1)) * (15 - i));
-            }
-            
-            rest = sum % 11;
-            division = 11 - rest;
-            
-            if (division == 11){
-                division = 0;
-            }
+    // Validate CNS Prov
+    if ([7, 8, 9].includes(parseInt(numCns.substring(0, 1)))) {
 
-            if (division == 10){
-                
-                for(var i = 0; i < 11; i++) {
-                    sum += parseInt(pis.substring(i, (i + 1)) * (15 - i)) + ((15 - i) == 5 ? 2 : 0);
-                }
+      for (var i = 0; i < 15; i++) {
+        sum += parseInt(numCns.substring(i, (i + 1)) * (15 - i));
+      }
 
-                rest = sum % 11;
-                division = 11 - rest;
-                result = pis + '001' + division.toString();
-            } else{
-                result = pis + "000" + division.toString();
-            }
+      rest = sum % 11;
 
-            if (!(numCns == result)){
-                return false;
-            } else{
-                return true;
-            }
-        }
-
-        // Validate CNS Prov
-        if ( [7,8,9].includes(parseInt(numCns.substring(0,1))) ) {
-            
-            for(var i = 0; i < 15; i++) {
-                sum += parseInt(numCns.substring(i, (i + 1)) * (15 - i));
-            }
-            
-            rest = sum % 11;
-
-            if (rest != 0){
-                return false;
-            } else{
-                return true;
-            }
-        }
-    });
+      if (rest != 0) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  });
 }
--- plugins/date-formatting.js
+++ plugins/date-formatting.js
@@ -4,32 +4,32 @@
 */
 
 export default ({ app }, inject) => {
-    // Inject $dateFormatting(date) in Vue, context and store.
+  // Inject $dateFormatting(date) in Vue, context and store.
 
-    inject('dateFormatting', (date, separator = '-', type = 'us') => {
-        if (date != undefined) {
-            let currenteparator = date.substring(0,10).replace(/\d/g, '')[0];
-            let dateArray = date.split(currenteparator);
-            let day, month, year, newDate;
+  inject('dateFormatting', (date, separator = '-', type = 'us') => {
+    if (date != undefined) {
+      let currenteparator = date.substring(0, 10).replace(/\d/g, '')[0];
+      let dateArray = date.split(currenteparator);
+      let day, month, year, newDate;
 
-            month = zeroLeft(parseInt(dateArray[1]))
-            if (dateArray[0].length == 4) {
-                year = parseInt(dateArray[0]);
-                day = zeroLeft(parseInt(dateArray[2]));
-            } else {
-                year = parseInt(dateArray[2]);
-                day = zeroLeft(parseInt(dateArray[0]));
-            }
+      month = zeroLeft(parseInt(dateArray[1]))
+      if (dateArray[0].length == 4) {
+        year = parseInt(dateArray[0]);
+        day = zeroLeft(parseInt(dateArray[2]));
+      } else {
+        year = parseInt(dateArray[2]);
+        day = zeroLeft(parseInt(dateArray[0]));
+      }
 
 
-            newDate = type == 'us' ? [year, month, day].join(separator) : [day, month, year].join(separator)
+      newDate = type == 'us' ? [year, month, day].join(separator) : [day, month, year].join(separator)
 
-            return newDate
-            
-        }
-    });
+      return newDate
 
-    function zeroLeft(num) {
-        return num < 10 ? '0'+num : num
     }
+  });
+
+  function zeroLeft(num) {
+    return num < 10 ? '0' + num : num
+  }
 }
